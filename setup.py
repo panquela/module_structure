0c00407174f8c8a5df9a5e874d894afb9d9eae20
Pedro Anquela <pedro.anquela@somosierratech.com>
2022-10-05 09:02:56 +0200

    Commit 9. Refactoring setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 MAJOR = 1
 MINOR = 0
 PATCH = 0
-COMMIT = 8
+COMMIT = 9
 
 # from main import *
 
@@ -19,22 +19,24 @@ __author__  = 'Universidad Francisco de Vitoria'
 __name__    = 'module_structure'
 
 VERSION = __version__
+AUTHOR = __author__
+NAME = __name__
 
 setup(
-    name='module_structure',
-    version=VERSION,
-    description='Brief description of your package',
-    author='Pedro Anquela',
-    author_email='pedro.anquela@ufv.es',
-    license='MIT',
-    python_requires='>=3.9.5',
-    packages=find_packages(),
-    include_package_data=True,
-    package_data={'': ['resources/*.csv','resources/clusters/*.csv']},
-    install_requires=[
-        'pandas',
-        'numpy',
-        'tensorflow-gpu',
-        'torch'
-        ]
-)
+    name                    = NAME,
+    version                 = VERSION,
+    description             = 'Brief description of your package',
+    author                  = AUTHOR,
+    author_email            = 'pedro.anquela@ufv.es',
+    license                 = 'MIT',
+    python_requires         = '>=3.9.5',
+    packages                = find_packages(),
+    include_package_data    = True,
+    package_data            = {'': ['resources/*.csv','resources/clusters/*.csv']},
+    install_requires        = [
+                                'pandas',
+                                'numpy',
+                                'tensorflow-gpu',
+                                'torch'
+                                ]
+     )
